48c33f22aa53b9a29c5739ed15c00e0562d74dbe
Cem Hurturk <cem@hurturk.com>
2023-08-19 13:27:13 +0300

    files and navigation are created

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -2,13 +2,19 @@ import {defineConfig} from 'vitepress'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-    lastUpdated: true,
     externalLinkIcon: false,
     land: 'en-US',
     title: "Octeth API Docs",
     description: "Octeth API Documentation",
     themeConfig: {
         // https://vitepress.dev/reference/default-theme-config
+        lastUpdated: {
+            text: "Updated at",
+            formatOptions: {
+                dateStyle: 'full',
+                timeStyle: 'medium'
+            }
+        },
         logo: {
             light: 'https://www.gitbook.com/cdn-cgi/image/width=256,dpr=2,height=40,fit=contain,format=auto/https%3A%2F%2F1273657825-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FziTRqi0uEeZcrlRGNIxU%252Flogo%252F51pD88PWPkgG4CbyUCGk%252FOcteth%2520-%2520Black.png%3Falt%3Dmedia%26token%3D5d0ca3e1-806e-433f-a896-56e2808bd011',
             dark: 'https://www.gitbook.com/cdn-cgi/image/width=256,dpr=2,height=40,fit=contain,format=auto/https%3A%2F%2F1273657825-files.gitbook.io%2F~%2Ffiles%2Fv0%2Fb%2Fgitbook-x-prod.appspot.com%2Fo%2Fspaces%252FziTRqi0uEeZcrlRGNIxU%252Flogo%252FBXWJ7UzTtEZz7FCPAHTr%252FOcteth%2520-%2520White.png%3Falt%3Dmedia%26token%3Ded68b0c4-ccb3-421d-89de-a090039e0692',
@@ -16,43 +22,57 @@ export default defineConfig({
         },
         siteTitle: 'API Docs',
         nav: [
-            {text: 'Home', link: '/'},
+            {text: 'Homepage', link: '/'},
             {
-                text: "Drop",
+                text: "Resources",
                 items: [
-                    {text: 'Item A', link: '#1', target: '_blank', rel: 'nofollow'},
-                    {text: 'Item B', link: '#2'},
-                    {text: 'Item C', link: '#3'}
+                    {text: 'Client Area', link: 'https://my.octeth.com/', target: '_blank', rel: 'nofollow'},
+                    {text: 'Help Portal', link: 'https://help.octeth.com/', target: '_blank', rel: 'nofollow'},
+                    {text: 'Contact Us', link: 'mailto:support@octeth.com'}
                 ]
-            },
-            {text: 'Examples', link: '/markdown-examples'},
-            {text: 'Contact Us', link: 'mailto:support@octeth.com'}
+            }
         ],
 
         sidebar: [
             {
-                text: 'Getting Started',
+                text: 'WELCOME',
+                collapsed: false,
                 items: [
-                    {text: 'Homepage', link: '/'},
-                    {text: 'Getting Started', link: '/getting-started'}
+                    {text: 'Getting Started', link: '/getting-started'},
+                    {text: 'Authorization', link: '/authorization'},
+                    {text: 'Error Handling', link: '/error-handling'},
+                    {text: 'Support', link: '/support'}
                 ]
             },
             {
-                text: 'API Endpoints',
-                collapsed: true,
+                text: 'API REFERENCE',
+                collapsed: false,
                 items: [
-                    {text: 'Authentication', link: '/1'}
+                    {text: 'Administrators', link: '/api-reference/administrators'},
+                    {text: 'Users', link: '/api-reference/users'},
+                    {text: 'Subscriber Lists', link: '/api-reference/subscriber-lists'},
+                    {text: 'Custom Fields', link: '/api-reference/custom-fields'},
+                    {text: 'Segments', link: '/api-reference/segments'},
+                    {text: 'Tags', link: '/api-reference/tags'},
+                    {text: 'Subscribers', link: '/api-reference/subscribers'},
+                    {text: 'Suppression Lists', link: '/api-reference/suppression-lists'},
+                    {text: 'Journeys', link: '/api-reference/journeys'},
+                    {text: 'Email Gateway', link: '/api-reference/email-gateway'},
+                    {text: 'Email Templates', link: '/api-reference/email-templates'},
+                    {text: 'Email Contents', link: '/api-reference/email-contents'},
+                    {text: 'Email Campaigns', link: '/api-reference/email-campaigns'},
+                    {text: 'System Management', link: '/api-reference/system-management'},
                 ]
             }
         ],
 
         socialLinks: [
-            {icon: 'github', link: 'https://octeth.com'}
+            {icon: 'github', link: 'https://github.com/octeth/octeth-api-docs'}
         ],
 
         footer: {
-            message: 'This is a <a href="https://octeth.com" target="_blank">message</a>',
-            copyright: "Copyright 50SAAS LLC. All rights reserved."
+            message: 'Any questions? <a href="mailto:hello@octeth.com">Contact us</a>.',
+            copyright: "&copy;Copyright 50SAAS LLC. All rights reserved."
         }
     }
 })
--- getting-started.md
+++ getting-started.md
@@ -6,7 +6,7 @@ layout: doc
 
 The Octeth API is designed to extend the functionality of our software, allowing developers to integrate Octeth with third-party systems quickly and efficiently.
 
-## Introduction <Badge type="info" text="default" />
+## Introduction
 
 Welcome to the Octeth API documentation. Octeth is an on-premise, enterprise-grade email marketing software designed to provide powerful audience management and email marketing features. It is built to run on customer infrastructure, offering a robust solution for businesses looking to streamline their email marketing efforts.
 
@@ -17,5 +17,3 @@ This documentation aims to provide a comprehensive guide to using the Octeth API
 > Please note that while there are no specific terms or concepts you need to be familiar with before using the API, a basic understanding of APIs and HTTP methods will be beneficial. Also, there are no major limitations or considerations to be aware of as you use the Octeth API.
 
 We hope this documentation helps you make the most of the Octeth API. Happy coding!
-
-Let's see how it goes now: <Badge type="tip" text="default" />
